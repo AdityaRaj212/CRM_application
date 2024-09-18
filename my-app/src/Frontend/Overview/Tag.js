2cfcf1680c01c6834b52ac9973520c6478e7bb21
Akshat Srivastava <103340547+LostNSeeker@users.noreply.github.com>
2024-09-18 15:44:54 +0530

    Delete my-app directory

--- my-app/src/Frontend/Overview/Tag.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import './Tag.css'; // Assuming you're using a separate CSS file for styling
-
-const Tag = ({ text }) => {
-  return <span className="tag">{text}</span>;
-};
-
-export default Tag;
